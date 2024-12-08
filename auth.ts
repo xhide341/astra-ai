ec0cc6c740362b82b928124630a21f2490d33dec
xhide341 <shawnehgn10@gmail.com>
2024-12-08 21:22:35 +0800

    feat: enhance authentication with user roles and session management
    - Introduced UserRole enum in `schema.prisma` to define user roles (ADMIN, USER).
    - Updated `User` model to include a role field with a default value of USER.
    - Enhanced `auth.ts` to include role management in session and JWT callbacks, allowing user roles to be stored and retrieved during authentication.
    - Implemented logic to fetch user roles from the database and attach them to the session.
    
    These changes improve the authentication system by incorporating user roles, enabling role-based access control in the application.

--- auth.ts
+++ auth.ts
@@ -1,10 +1,49 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "@/lib/db";
 import authConfig from "./auth.config";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
 
-export const { handlers, auth, signIn, signOut } = NextAuth(
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            role: UserRole;
+        } & DefaultSession["user"];
+    }
+}
+
+export const {
+    handlers,
+    auth,
+    signIn,
+    signOut
+} = NextAuth(
     {
+        callbacks: {
+            async session({ session, token }) {
+                if (token.sub && session.user) {
+                    session.user.id = token.sub;
+                }
+
+                if (token.role && session.user) {
+                    session.user.role = token.role as UserRole;
+                }
+
+                return session;
+            },
+            async jwt ({ token }) {
+                if (!token.sub) return token;
+
+                const existingUser = await getUserById(token.sub);
+                if (!existingUser) return token;
+
+                token.role = existingUser.role;
+
+                return token;
+            }
+        },
         adapter: PrismaAdapter(db),
         session: {
             strategy: "jwt",
