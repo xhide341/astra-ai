8501138516d976ef4ba2f55db0d6084b414d414a
xhide341 <shawnehgn10@gmail.com>
2024-12-08 15:45:45 +0800

    chore: update dependencies and remove unused auth components
    - Added `next-auth` version 5.0.0-beta.25 and `@auth/core` version 0.37.4 to `package.json`.
    - Updated `pnpm-lock.yaml` to reflect new dependencies and their versions.
    - Deleted unused authentication layout and page components from the `app/(auth)` directory.

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import db from "@/lib/db"
+import authConfig from "./auth.config"
+
+export const { handlers, auth } = NextAuth(
+    {
+        adapter: PrismaAdapter(db),
+        session: {
+            strategy: "jwt",
+        },  
+        ...authConfig
+    }
+)
---
+++ routes.ts
@@ -0,0 +1,36 @@
+/**
+ * Routes that are accessible to everyone, including unauthenticated users.
+ * @type {string[]}
+**/  
+export const publicRoutes = [
+    "/",
+];
+
+/**
+ * Routes that are only accessible to authenticated users.
+ * @type {string[]}
+**/  
+export const protectedRoutes = [
+    "/settings"
+];
+
+/**
+ * Routes that are related to authentication.
+ * @type {string[]}
+**/  
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register"
+];
+
+/**
+ * The prefix for API routes that are related to authentication.
+ * @type {string}
+**/  
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default login redirect route.
+ * @type {string}
+**/  
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
