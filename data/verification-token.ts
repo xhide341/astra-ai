2646643238eeb85fb5eecca2c060cd970272ea06
xhide341 <shawnehgn10@gmail.com>
2024-12-19 15:47:46 +0800

    refactor: move token functions to tokens.ts

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -1,53 +1,6 @@
 import db from "@/lib/db";
 import crypto from "crypto";
 
-export const createVerificationToken = async (identifier: string, token: string, expires: Date) => {
-  try {
-    await db.verificationToken.create({
-      data: {
-        identifier,
-        token,
-        expires
-    },
-    });
-  } catch (error) {
-    console.error("Error creating verification token:", error);
-  }
-};
-
-export const getVerificationTokenByIdentifier = async (identifier: string, token: string) => {
-  try {
-    const verificationToken = await db.verificationToken.findUnique({
-      where: { 
-        identifier_token: {
-          identifier,
-          token
-        }
-      }
-    });
-
-    return verificationToken;
-  } catch (error) {
-    console.error("Error getting verification token:", error);
-    return null;
-  }
-};
-
-export const getVerificationTokenByToken = async (token: string) => {
-  try {
-    const verificationToken = await db.verificationToken.findFirst({
-      where: {
-        token: token
-      }
-    });
-
-    return verificationToken;
-  } catch (error) {
-    console.error("Error getting verification token:", error);
-    return null;
-  }
-};
-
 export const generateVerificationToken = async (identifier: string): Promise<string> => {
   const token = crypto.randomBytes(32).toString('hex');
   const expires = new Date(Date.now() + 1000 * 60 * 60 * 1); // 1 hour
