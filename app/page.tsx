84d108fc1b999d7961acc0c5d68027ce796395c9
xhide341 <shawnehgn10@gmail.com>
2025-01-18 19:14:02 +0800

    fix: remove dark classes in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { StarIcon } from "@heroicons/react/24/solid";
 import { Sparkles } from "@/components/ui/sparkles";
-import { ThemeToggle } from "@/components/ui/theme-toggle";
+
 
 
 export default function Home() {
@@ -17,35 +16,27 @@ export default function Home() {
             <Button asChild variant="secondary">
               <Link href="/auth/register">Get Started</Link>
             </Button>
-            <ThemeToggle />
           </nav>
         </div>
       </header>
 
-      <main className="z-10 flex flex-col items-center justify-center w-full px-6 lg:px-8">
-        {/* hero section */}
-        <section className="flex items-center justify-center w-full max-w-7xl mx-auto sm:py-36">
-          <div className="flex flex-col">
-            <div className="flex items-center gap-4 text-primary-color">
-              <Button asChild variant="default">
-                <Link href="/explore">Start Exploring</Link>
-              </Button>
-            </div>
+      <main className="z-10 flex flex-1 flex-col items-center justify-between w-full px-6 lg:px-8">
+        <section className="flex flex-col items-center w-full mx-auto pt-20 flex-1">
+          <div className="text-center">
             <h2 className="text-3xl/[1.25] sm:text-4xl/[1.25] mb-4 font-bold text-primary-color dark:text-white/80 max-w-2xl leading-normal">
               Just enter a topic
             </h2>
             <p className="text-md text-secondary-color dark:text-white/80 max-w-[500px] mb-6">
               and let two AI minds discuss it in depth.
             </p>
-            <div className="relative -mt-32 h-96 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,var(--primary-color),transparent_40%)] before:opacity-40 after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-secondary-color after:bg-zinc-900">
+          </div>
+          <div className="fixed bottom-0 left-0 right-0 h-96 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,var(--primary-color),transparent_40%)] before:opacity-40 after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-secondary-color after:bg-zinc-900">
             <Sparkles
-                density={1200}
-                className="absolute inset-x-0 bottom-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
-              />
-            </div>
+              density={1200}
+              className="absolute inset-x-0 bottom-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
+            />
           </div>
         </section>
-
       </main>
     </div>
   );
